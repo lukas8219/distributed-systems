55112bab6c3d10715ef17fad4d98247289d6e91c
Lucas Polesello <lucas.c4d@gmail.com>
2025-12-28 00:21:15 -0300

    feat(maelstrom/broadcast-c): multi node solution

--- maelstrom/maelstrom-broadcast/main.go
+++ maelstrom/maelstrom-broadcast/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
+	"encoding/json"
+	"log"
+	"strings"
 
-  maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-  "sync"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main(){
@@ -18,21 +20,33 @@ func main(){
     return nil
   })
 
+
+  /*
+I need to create a sync mechanism since `Send` can fail and it doesnt seem like I can use SyncRPC
+Maybe fetch neighbors state each ~50ms or something
+I can still spend some time in making SyncRPC work (as, although not performant) is the more intuitive way
+Rather not optimize before the real challenges
+  */
+
   n.Handle("broadcast", func(msg maelstrom.Message) error {
-    lock.Lock()
-    defer lock.Unlock()
     var body map[string]any
     if err := json.Unmarshal(msg.Body, &body); err != nil {
       return err
     }
 
     message := body["message"].(float64)
+    lock.Lock()
     if _, ok := state[message]; ok {
-      return nil
+      lock.Unlock()
+      return n.Reply(msg, map[string]any{"type":"broadcast_ok"})
     }
     state[message] = struct{}{}
+    lock.Unlock()
 
     for _, neighbor := range neighbors {
+      if strings.EqualFold(neighbor, msg.Src) {
+        continue
+      }
       n.Send(neighbor, map[string]any{ "type": "broadcast", "message": message })
     }
 
@@ -49,7 +63,7 @@ func main(){
     messageNeighbors := body["topology"].(map[string]any)[n.ID()]
 
     nnbrs := make([]string, 0)
-    for _, v := range messageNeighbors.([]interface{}) {
+    for _, v := range messageNeighbors.([]any) {
       nnbrs = append(nnbrs, v.(string))
     }
     neighbors = nnbrs
@@ -64,7 +78,7 @@ func main(){
       return err
     }
     messages := make([]float64, len(state))
-    for v, _ := range state {
+    for v := range state {
       messages = append(messages, v)
     }
     return n.Reply(msg, map[string]any{
