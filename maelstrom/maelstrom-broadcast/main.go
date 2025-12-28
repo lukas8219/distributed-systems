c347e19df72b4fef113638059f13f2b8b8beb325
Lucas Polesello <lucas.c4d@gmail.com>
2025-12-27 22:55:53 -0300

    feat(maelstrom-broadcast): passing multi-node scenario broadcast-b

--- maelstrom/maelstrom-broadcast/main.go
+++ maelstrom/maelstrom-broadcast/main.go
@@ -1,47 +1,75 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+  "encoding/json"
+  "log"
 
-	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+  maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+  "sync"
 )
 
 func main(){
   n := maelstrom.NewNode()
-  var state = make([]float64, 0)
+  state := map[float64]struct{}{}
+  var neighbors = make([]string, 0)
+  var lock sync.RWMutex
+
+  n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+    return nil
+  })
 
   n.Handle("broadcast", func(msg maelstrom.Message) error {
-    // Unmarshal the message body as an loosely-typed map.
+    lock.Lock()
+    defer lock.Unlock()
     var body map[string]any
     if err := json.Unmarshal(msg.Body, &body); err != nil {
-        return err
+      return err
     }
 
-    bodyMessage := body["message"]
-    state = append(state, bodyMessage.(float64))
+    message := body["message"].(float64)
+    if _, ok := state[message]; ok {
+      return nil
+    }
+    state[message] = struct{}{}
+
+    for _, neighbor := range neighbors {
+      n.Send(neighbor, map[string]any{ "type": "broadcast", "message": message })
+    }
+
     return n.Reply(msg, map[string]any{
-    "type": "broadcast_ok",
+      "type": "broadcast_ok",
     })
   })
   n.Handle("topology", func(msg maelstrom.Message) error {
     var body map[string]any
     if err := json.Unmarshal(msg.Body, &body); err != nil {
-        return err
+      return err
     }
 
+    messageNeighbors := body["topology"].(map[string]any)[n.ID()]
+
+    nnbrs := make([]string, 0)
+    for _, v := range messageNeighbors.([]interface{}) {
+      nnbrs = append(nnbrs, v.(string))
+    }
+    neighbors = nnbrs
+
     return n.Reply(msg, map[string]any{
-    "type": "topology_ok",
+      "type": "topology_ok",
     })
   })
   n.Handle("read", func(msg maelstrom.Message) error {
     var body map[string]any
     if err := json.Unmarshal(msg.Body, &body); err != nil {
-        return err
+      return err
+    }
+    messages := make([]float64, len(state))
+    for v, _ := range state {
+      messages = append(messages, v)
     }
     return n.Reply(msg, map[string]any{
       "type": "read_ok",
-      "messages": state,
+      "messages": messages,
     })
   })
 
