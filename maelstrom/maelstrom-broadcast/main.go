add4c3e09d498ecbf06af2ce2968cb25af118bda
Lucas Weis Polesello <75316334+lukas8219@users.noreply.github.com>
2025-12-29 18:19:37 -0300

    feat(maelstrom/broadcast): variation `D` (#2)
    * msg-per-op:22, median 230, max: 1312
    
    * Adjusted for further profiling

--- maelstrom/maelstrom-broadcast/main.go
+++ maelstrom/maelstrom-broadcast/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"flag"
 	"log"
+	"os"
+	"runtime/pprof"
 	"strings"
 	"time"
 
@@ -13,153 +17,230 @@ import (
 )
 
 type Neighbor struct {
-  id string
-  lastSyncedVersion float64
+	id                string
+	lastSyncedVersion uint64
 }
 
 func now() int64 {
-  return time.Now().UnixNano()
+	return time.Now().UnixNano()
 }
 
-func main(){
-  n := maelstrom.NewNode()
-  index := map[float64]struct{}{} //this would be reconstructed from the WAL/state variable
-  state := make([]float64, 0)
-  var neighbors = map[string]*Neighbor{}
-  var lock sync.RWMutex
+type Sync struct {
+	Type  string `json:"type"`
+	Delta uint64 `json:"delta"`
+}
 
-  n.Handle("sync", func(msg maelstrom.Message) error {
-    lock.Lock()
-    defer lock.Unlock()
-    var body map[string]any
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-      return err
-    }
-    currentState := float64(len(state))
-    dt := body["delta"].(float64)
-    if currentState == dt {
-      // all good
-      return nil
-    }
-    if currentState < dt {
-      panic("Invalid state")
-    }
-    missingMessages := state[int(dt):int(currentState)] 
-    return n.Reply(msg, map[string]any {
-      "type": "sync_ok",
-      "messages_delta": missingMessages,
-      "delta": currentState,
-    } )
-  })
-  n.Handle("sync_ok", func(msg maelstrom.Message) error {
-    return nil
-  })
+type SyncOk struct {
+	Type          string   `json:"type"`
+	MessagesDelta []uint64 `json:"messages_delta"`
+	Delta         uint64   `json:"delta"`
+}
 
-  n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
-    return nil
-  })
+type Broadcast struct {
+	Type    string `json:"type"`
+	Message uint64 `json:"message"`
+}
 
+type BroadcastOk struct {
+	Type      string `json:"type"`
+	InReplyTo uint64 `json:"in_reply_to,omitempty"`
+}
 
-  /*
-I need to create a sync mechanism since `Send` can fail and it doesnt seem like I can use SyncRPC
-Maybe fetch neighbors state each ~50ms or something
-I can still spend some time in making SyncRPC work (as, although not performant) is the more intuitive way
-Rather not optimize before the real challenges
-  */
+type Topology struct {
+	Type     string              `json:"type"`
+	Topology map[string][]string `json:"topology"`
+}
 
+type TopologyOk struct {
+	Type string `json:"type"`
+}
 
-  n.Handle("broadcast", func(msg maelstrom.Message) error {
-    var body map[string]any
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-      return err
-    }
+type Read struct {
+	Type string `json:"type"`
+}
 
-    message := body["message"].(float64)
-    lock.Lock()
-    defer lock.Unlock()
-    if _, ok := index[message]; ok {
-      return n.Reply(msg, map[string]any{"type":"broadcast_ok"})
-    }
-    index[message] = struct{}{}
-    state = append(state, message)
+type ReadOk struct {
+	Type     string   `json:"type"`
+	Messages []uint64 `json:"messages"`
+}
 
-    for _, neighbor := range neighbors {
-      if strings.EqualFold(neighbor.id, msg.Src) {
-        continue
-      }
-      n.Send(neighbor.id, map[string]any{ "type": "broadcast", "message": message })
-    }
+func enableCpuProfileIfNeeded() *os.File {
+	var cpuProfile = flag.String("profile", "", "cpu profile enabling")
+	flag.Parse()
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		return f
+	}
+	return nil
+}
 
-    return n.Reply(msg, map[string]any{
-      "type": "broadcast_ok",
-    })
-  })
-  n.Handle("topology", func(msg maelstrom.Message) error {
-    var body map[string]any
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-      return err
-    }
+func main() {
+	n := maelstrom.NewNode()
+	index := map[uint64]struct{}{} //this would be reconstructed from the WAL/state variable
+	state := make([]uint64, 0)
+	var neighbors = map[string]*Neighbor{}
+	var lock sync.RWMutex
+	var topologySynced = false
 
-    messageNeighbors := body["topology"].(map[string]any)[n.ID()]
+	calculateDelta := func(delta uint64) (uint64, []uint64, error) {
+		currentState := uint64(len(state))
+		if currentState == delta {
+			return 0, []uint64{}, nil
+		}
+		if currentState < delta {
+			return 0, []uint64{}, errors.New("Invalid state, skip")
+		}
+		missingMessages := state[delta:currentState]
+		return uint64(currentState), missingMessages, nil
+	}
 
-    for _, v := range messageNeighbors.([]any) {
-      nnb := v.(string)
-      neigh := &Neighbor{ id: nnb, lastSyncedVersion: 0}
-      neighbors[nnb] = neigh
-    }
+	sendBatch := func(nbId string, nblastSyncVersion uint64) {
+		delta, messages, err := calculateDelta(nblastSyncVersion)
+		if err != nil {
+			return
+		}
+		n.Send(nbId, SyncOk{Type: "sync_ok", Delta: delta, MessagesDelta: messages})
+	}
 
-    return n.Reply(msg, map[string]any{
-      "type": "topology_ok",
-    })
-  })
-  n.Handle("read", func(msg maelstrom.Message) error {
-    var body map[string]any
-    if err := json.Unmarshal(msg.Body, &body); err != nil {
-      return err
-    }
-    return n.Reply(msg, map[string]any{
-      "type": "read_ok",
-      "messages": state,
-    })
-  })
+	handleSyncOk := func(source string, msg maelstrom.Message) error {
+		lock.Lock()
+		defer lock.Unlock()
+		var syncOk SyncOk
+		if err := json.Unmarshal(msg.Body, &syncOk); err != nil {
+			return err
+		}
+		deltaMessage := syncOk.MessagesDelta
+		for _, m := range deltaMessage {
+			if _, ok := index[m]; ok {
+				continue
+			}
+			index[m] = struct{}{}
+			state = append(state, m)
+		}
+		neighbors[source].lastSyncedVersion = syncOk.Delta
+		return nil
+	}
 
-  bg := context.Background()
-  // This is a hard NO
-  go func(){
-    for range time.Tick(time.Millisecond * 250) {
-      for _, nb := range neighbors {
-        ctx, cancel := context.WithTimeout(bg, time.Second * 10)
-        defer cancel()
-        msg, err := n.SyncRPC(ctx, nb.id, map[string]any{
-          "type": "sync",
-          "delta": nb.lastSyncedVersion,
-        })
-        if err != nil {
-          continue
-        }
-        lock.Lock()
-        var body map[string]any
-        if err := json.Unmarshal(msg.Body, &body); err != nil {
-          continue
-        }
-        deltaMessage := body["messages_delta"].([]any)
-        for _, mAny := range deltaMessage {
-          m := mAny.(float64)
-          if _, ok := index[m]; ok {
-            continue
-          }
-          index[m] = struct{}{}
-          state = append(state, m)
-        }
-        neighbors[msg.Src].lastSyncedVersion = body["delta"].(float64)
-        lock.Unlock()
-      }
-    }
-  }()
+	n.Handle("sync", func(msg maelstrom.Message) error {
+		lock.Lock()
+		defer lock.Unlock()
+		var sync Sync
+		if err := json.Unmarshal(msg.Body, &sync); err != nil {
+			return err
+		}
+		delta, messagesDelta, err := calculateDelta(sync.Delta)
+		if err != nil {
+			return err
+		}
+		return n.Reply(msg, SyncOk{Type: "sync_ok", Delta: delta, MessagesDelta: messagesDelta})
+	})
+	n.Handle("sync_ok", func(msg maelstrom.Message) error {
+		return handleSyncOk(msg.Src, msg)
+	})
 
-  if err := n.Run(); err != nil {
-    log.Fatal(err)
-  }
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		return nil
+	})
 
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var broadcast Broadcast
+		if err := json.Unmarshal(msg.Body, &broadcast); err != nil {
+			return err
+		}
+
+		message := broadcast.Message
+		ok := BroadcastOk{Type: "broadcast_ok"}
+
+		lock.Lock()
+		defer lock.Unlock()
+		if _, exists := index[message]; exists {
+			return n.Reply(msg, ok)
+		}
+		index[message] = struct{}{}
+		state = append(state, message)
+
+		for _, neighbor := range neighbors {
+			if strings.EqualFold(neighbor.id, msg.Src) {
+				continue
+			}
+			//TODO: Batch each 50ms the Broadcast
+			// n.Send(neighbor.id, broadcast)
+		}
+
+		return n.Reply(msg, ok)
+	})
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var topology Topology
+		if err := json.Unmarshal(msg.Body, &topology); err != nil {
+			return err
+		}
+
+		messageNeighbors := topology.Topology[n.ID()]
+
+		for _, nnb := range messageNeighbors {
+			neigh := &Neighbor{id: nnb, lastSyncedVersion: 0}
+			neighbors[nnb] = neigh
+		}
+
+		topologySynced = true
+
+		return n.Reply(msg, TopologyOk{Type: "topology_ok"})
+	})
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var read Read
+		if err := json.Unmarshal(msg.Body, &read); err != nil {
+			return err
+		}
+		return n.Reply(msg, ReadOk{Type: "read_ok", Messages: state})
+	})
+
+	bg := context.Background()
+
+	isNeighborsReady := func() bool {
+		return topologySynced
+	}
+	// This is a hard NO
+	//TODO: maybe move into different WG to control 2 concurrent syncs
+	go func() {
+		for range time.Tick(time.Millisecond * 250) {
+			if !isNeighborsReady() {
+				continue
+			}
+			for _, nb := range neighbors {
+				ctx, cancel := context.WithTimeout(bg, time.Second*10)
+				defer cancel()
+				msg, err := n.SyncRPC(ctx, nb.id, Sync{Type: "sync", Delta: nb.lastSyncedVersion})
+				if err != nil {
+					continue
+				}
+				handleSyncOk(nb.id, msg)
+			}
+		}
+	}()
+
+	go func() {
+		for range time.Tick(time.Millisecond * 125) {
+			if !isNeighborsReady() {
+				continue
+			}
+			for _, nb := range neighbors {
+				sendBatch(nb.id, nb.lastSyncedVersion)
+			}
+		}
+	}()
+
+	f := enableCpuProfileIfNeeded()
+	if f != nil {
+		defer f.Close()
+		defer pprof.StopCPUProfile()
+	}
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
 
 }
